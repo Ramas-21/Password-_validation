e0f4b26866eb5c6245b7f2a8a32325d28d499001
Lawrence <lawrencehome21@gmail.com>
2023-09-02 11:12:13 -0700

    created an array of password requirements

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const passwordInput = document.querySelector(".pass-field input");
 const eyeIcon = document.querySelector(".pass-field i");
 
+// An array of password requirements
+const requirements = [
+    {regex: /.{8,}/, index: 0},
+    {regex: /[0-9]/, index: 1},
+    {regex: /[a-z]/, index: 2},
+    {regex: /[^A-Za-z0-9]/, index: 3},
+    {regex: /[A-Z]/, index: 4},
+]
+
+passwordInput.addEventListener("keyup", () =>{
+
+});
+
 eyeIcon.addEventListener("click", () =>{
     // These is to toggle the password input type btn "password" and "text"
     passwordInput.type = passwordInput.type === "password" ? "text" : "password";
