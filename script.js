1fceed62adff827ff49e672faad8a63cabca9319
Lawrence <lawrencehome21@gmail.com>
2023-09-02 11:04:54 -0700

    added a comment

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ eyeIcon.addEventListener("click", () =>{
     // These is to toggle the password input type btn "password" and "text"
     passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 
+    //these for updating the eye icon class based on the password input type
     eyeIcon.className = `fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
 });
