9722d502fdf1cbec175c1d6af88cfc01267e110b
Lawrence <lawrencehome21@gmail.com>
2023-09-02 11:03:31 -0700

    syntax bugs

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ eyeIcon.addEventListener("click", () =>{
     // These is to toggle the password input type btn "password" and "text"
     passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 
-    eyeIcon.className = 'fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}';
+    eyeIcon.className = `fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
 });
